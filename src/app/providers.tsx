09202c057802eae0f7adefb551f954a565b4930e
Tuan980Blue <tuanmeo980provip@gmail.com>
2025-10-23 23:16:20 +0700

    add broadcast Channel

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,9 +3,16 @@
 import { AuthProvider } from "@/context/AuthContext";
 import {Provider} from "react-redux";
 import {store} from "@/store/store";
+import {useEffect} from "react";
+import {initBroadcastSync} from "@/store/syncState";
 
 export default function AppProviders({ children }: { children: React.ReactNode }) {
-  return (
+    useEffect(() => {
+        //Thiết lập kênh BroadcastChannel, lắng nghe và gửi message giữa tab
+        initBroadcastSync(store);
+    }, []);
+
+    return (
       <Provider store={store}>
           <AuthProvider>
               {children}
